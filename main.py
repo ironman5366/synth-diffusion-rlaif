f54032e04f96a9dde5d90e9c3af32cbd1373241e
Will Beddow <will@willbeddow.com>
2024-02-24 14:47:06 -0800

    Compare prompts

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ Basic idea:
 from torch.utils.data import DataLoader
 from datasets import load_dataset
 from pathlib import Path
+from collections import defaultdict
 from tqdm import tqdm
-
 
 from accelerate import Accelerator
 from transformers import AutoModelForCausalLM, AutoTokenizer
@@ -69,36 +69,51 @@ loader = DataLoader(dataset, batch_size=1,
 agree_with_human = 0
 total = 0
 
+constraint_suffix =("Output '1' if the first image is better, '2' if the second image is better. "
+                    "Only output '1' or '2', do not output anything else")
+
+def base_caption(caption: str):
+    return f"Which of these images is best described by the caption '{caption}'? {constraint_suffix}"
+
+def base_no_caption(_caption: str):
+    return f"Which of these images has better aesthetic quality?. {constraint_suffix}"
+
+prompt_options = {
+    "base_caption": base_caption,
+    "base_no_caption": base_no_caption
+}
+
+prompt_accuracy = defaultdict(int)
+
 for batch in tqdm(loader):
     # TODO: batch this later
     for it in batch:
+        for prompt_key, prompt_fn in prompt_options:
+            caption = it["caption"]
+            prompt = prompt_fn(caption)
 
-        caption = it["caption"]
+            print(f"Comparing images {it['image_0_path']} and {it['image_1_path']} with {prompt}")
 
-        prompt = (f"Which of these images has better aesthetic quality, and best fits the prompt, '{caption}'. "
-                  f"Output '1' if the first image is better, '2' if the "
-                  f"second image is better. Only output '1' or '2', do not output anything else")
+            response = label_slice(
+                prompt,
+                image_paths=[it["image_0_path"], it["image_1_path"]],
+            )
 
-        print(f"Comparing images {it['image_0_path']} and {it['image_1_path']} with {prompt}")
+            if it["image_0_uid"] == it["best_image_uid"]:
+                better_image = "1"
+            else:
+                better_image = "2"
 
-        response = label_slice(
-            prompt,
-            image_paths=[it["image_0_path"], it["image_1_path"]],
-        )
+            print(response)
+            assert response in {"1", "2"}, "AI Bad :("
 
-        if it["image_0_uid"] == it["best_image_uid"]:
-            better_image = "1"
-        else:
-            better_image = "2"
+            if response == better_image:
+                prompt_accuracy[prompt_key] += 1
 
-        print(response)
-        assert response in {"1", "2"}, "AI Bad :("
+            print(f"AI response: {response}, human response: {better_image}, agree: {response == better_image}")
 
-        if response == better_image:
-            agree_with_human += 1
+            total += 1
+    break
 
-        print(f"AI response: {response}, human response: {better_image}, agree: {response == better_image}")
-
-        total += 1
-
-print(f"Agree with human: {agree_with_human}, total: {total}, percent: {agree_with_human/total}")
+for prompt_key, accuracy in prompt_accuracy.items():
+    print(f"Prompt {prompt_key} accuracy: {accuracy} / {total}, {accuracy / total}")
